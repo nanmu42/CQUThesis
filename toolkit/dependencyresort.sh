bb06cfd1cd5533acc164a9af5dd8eff8627f341d
nanmu42 <i@nanmu.me>
2016-06-20 23:33:35 +0800

    Revert "暂时先支持Linux"
    This reverts commit 7b6fcacb880b11d6c397b842c7d110ba6bdf6e3c.

--- toolkit/dependencyresort.sh
+++ toolkit/dependencyresort.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
-# todo: make the dependency sorting out process active.
+# To use texliveonfly to sort out the dependencies.
+# Run once. When done, pass everything to latexmk
+
+./toolkit/texliveonfly.py cquthesis.dtx
+./toolkit/texliveonfly.py main.tex
