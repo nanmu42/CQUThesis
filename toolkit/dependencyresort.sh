3e5aafa474fa7ce3aba554154a48efba858fb25c
nanmu42 <i@nanmu.me>
2016-06-20 20:49:04 +0800

    修正了程序路径

--- toolkit/dependencyresort.sh
+++ toolkit/dependencyresort.sh
@@ -3,5 +3,5 @@
 # To use texliveonfly to sort out the dependencies.
 # Run once. When done, pass everything to latexmk
 
-texliveonfly.py ../cquthesis.dtx
-texliveonfly.py ../main.tex
+./toolkit/texliveonfly.py cquthesis.dtx
+./toolkit/texliveonfly.py main.tex
